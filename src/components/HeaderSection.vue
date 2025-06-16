bf6506df6427ce3050d6db2cc8c747b6b2b05fb8
Ogün Usluer <ogunusluer0@gmail.com>
2025-06-16 20:04:22 +0300

    Update HeaderSection.vue

--- src/components/HeaderSection.vue
+++ src/components/HeaderSection.vue
@@ -2,7 +2,8 @@
   <section data-aos="fade-up" :class="props.textColor" class="w-full px-4 mb-6 text-center">
     <decoration class="fill-amber-400 mx-auto w-3/12 mb-3"></decoration>
     <h1 class="satisfy-font text-4xl font-medium mb-5">{{ props.title }}</h1>
-    <p class="text-sm">{{ subtitle }}</p>
+    <p class="text-sm">Tüm sevenlerimizi düğünümüze bekleriz,
+Gelemeyenleri dualarıyla yanımızda isteriz.</p>
   </section>
 </template>
 
