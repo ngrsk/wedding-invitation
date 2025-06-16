aeaacdb4df14d82e76d6bd6aed9c665532a4add5
Ogün Usluer <ogunusluer0@gmail.com>
2025-06-16 19:15:03 +0300

    Update MainCover.vue

--- src/components/MainCover.vue
+++ src/components/MainCover.vue
@@ -3,7 +3,7 @@
     <section class="container-section h-screen grid content-end">
       <div 
         class="animate__animated animate__fadeInUpBig text-gray-100 text-center bg-gray-800 bg-opacity-50 backdrop-blur-sm rounded-t-3xl px-6 py-10">
-        <p>Pernikahan</p>
+        
         <h1 class="tangerine-font text-title font-bold">{{ couples.join(' & ') }}</h1>
         <decoration class="fill-amber-400 mx-auto w-3/12 mb-3"></decoration>
         <p class="text-sm">
