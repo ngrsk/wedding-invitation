5509192df7b2e706e349fa4d54450db43b9c5d64
Ogün Usluer <ogunusluer0@gmail.com>
2025-06-16 21:24:22 +0300

    Update Countdown.vue

--- src/components/Countdown.vue
+++ src/components/Countdown.vue
@@ -44,7 +44,7 @@ const createTimer = (target, container, cb) => {
 }
 
 const timer = setInterval(() => {
-  createTimer(new Date('2022-08-04 16:00').getTime(), countdown, () => {
+  createTimer(new Date('2025-08-04 16:00').getTime(), countdown, () => {
     clearInterval(timer)
   })
 }, 1000)
