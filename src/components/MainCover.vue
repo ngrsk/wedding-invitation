674f7f5fb0c8b4024966687df57247577473680d
Ogün Usluer <ogunusluer0@gmail.com>
2025-06-21 00:07:15 +0300

    Update MainCover.vue

--- src/components/MainCover.vue
+++ src/components/MainCover.vue
@@ -7,7 +7,13 @@
         <h1 class="tangerine-font text-title font-bold" style="font-size:44px!important">{{ couples.join(' & ') }}</h1>
         <decoration class="fill-amber-400 mx-auto w-3/12 mb-3"></decoration>
         <p class="text-sm">
-          <p class="font-medium">Bu Mutlu Günümüzde Sizleride Yanımızda Görmek Dileğiyle..</p>
+          <p class="font-medium">
+            Aşkla çıktık yola❤ </br>
+Hasret erdi vuslata💑</br>
+Menzilimiz sonsuza🧿</br>
+Ey Dostlar, kulak verin nidamıza📣</br>
+Bu mutlu günümüzde olun Sizler de yanımızda🇹🇷  
+          </p>
           <small>04 Ağustos 2025 18:00</small>
         </p>
       </div>
